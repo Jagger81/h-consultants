ca37f066e25803fb2e21d8cb16ef0b6612afb9fa
Eddie Jagger <eddiejcranmer@gmail.com>
2019-01-27 19:36:00 +0000

    Product URL's complete - page content to be created

--- products/models.py
+++ products/models.py
@@ -3,15 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 
-fs = FileSystemStorage(location='/static/img/')
-
 #create your models here.
 class Product(models.Model):
     category = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(storage=fs)
+    image = models.ImageField(upload_to='static/img')
 
     def __str__(self):
         return self.name
